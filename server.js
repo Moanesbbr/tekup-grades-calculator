a34fc52d8760c8d132597556e38406a8c31ae17b
ben brahim moanes <moanesbbr@gmail.com>
2025-07-23 14:41:44 +0100

    fixes

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the main calculator page at root
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'bookmarklet.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app; 
