e38889955e0f2124ab0ea389b19845f516aef8c7
ben brahim moanes <moanesbbr@gmail.com>
2025-07-23 15:40:15 +0100

    end

--- server.js
+++ server.js
@@ -2,8 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Add error handling for static files
-app.use(express.static(path.join(__dirname, 'public'), { fallthrough: true }));
+// Add CORS headers for development
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public'), {
+    setHeaders: (res, filePath, stat) => {
+        if (filePath.endsWith('.js')) {
+            res.set({
+                'Content-Type': 'application/javascript; charset=utf-8',
+                'Cache-Control': 'no-cache'
+            });
+        }
+    }
+}));
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'public', 'bookmarklet.html'), (err) => {
@@ -11,6 +26,11 @@ app.get('/', (req, res, next) => {
       next(err);
     }
   });
+});
+
+app.get('/assets/main.js', (req, res) => {
+    res.type('application/javascript');
+    res.sendFile(path.join(__dirname, 'public', 'assets', 'main.js'));
 });
 
 // Error handling middleware
