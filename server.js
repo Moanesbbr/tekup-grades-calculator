8bc33ab19128e26718f7ba3ae95c0d25e897f9cf
ben brahim moanes <moanesbbr@gmail.com>
2025-07-23 14:33:16 +0100

    add vercel config

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,6 +11,4 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'bookmarklet.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Tekup Grades Calculator running at http://localhost:${PORT}`);
-}); 
+module.exports = app; 
