1710fb779dd55df9ac2ce6deb83d2ac65c610409
ben brahim moanes <moanesbbr@gmail.com>
2025-07-23 14:56:49 +0100

    fixes

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Add error handling for static files
+app.use(express.static(path.join(__dirname, 'public'), { fallthrough: true }));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'bookmarklet.html'));
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'public', 'bookmarklet.html'), (err) => {
+    if (err) {
+      next(err);
+    }
+  });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Something broke!');
 });
 
 const port = process.env.PORT || 3000;
@@ -13,4 +24,4 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-module.exports = app; 
+module.exports = app;
